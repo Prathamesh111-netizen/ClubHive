143114f1290420479ba4927048d6207989e816c1
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-02-04 21:59:52 +0530

    tc

--- server/controllers/approvalControllers.js
+++ server/controllers/approvalControllers.js
@@ -3,52 +3,64 @@ import Approval from "../models/Approval.model.js";
 dotenv.config();
 
 const RequestApproveEvent = async (req, res, next) => {
-  const { eventId } = req.params;
-  const { users } = req.body;
-  console.log(users);
-  users.map((userId) => Approval.create({ eventId, userId }));
+  try {
+    const { eventId } = req.params;
+    const { users } = req.body;
+    console.log(users);
+    users.map((userId) => Approval.create({ eventId, userId }));
 
-  res.status(201).json({
-    success: true,
-    message: "Approval Request sent to all users",
-  });
+    res.status(201).json({
+      success: true,
+      message: "Approval Request sent to all users",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const ApproveEvent = async (req, res, next) => {
-  const { eventId } = req.params;
-  const { userId } = req.body;
-  const approval = await Approval.findOne({ eventId, userId });
-  if (!approval) {
-    return res.status(404).json({
-      success: false,
-      message: "Approval not found",
+  try {
+    const { eventId } = req.params;
+    const { userId } = req.body;
+    const approval = await Approval.findOne({ eventId, userId });
+    if (!approval) {
+      return res.status(404).json({
+        success: false,
+        message: "Approval not found",
+      });
+    }
+
+    const UpdatedApproval = await Approval.findByIdAndUpdate(
+      approval._id,
+      { approvalStatus: "Approved" },
+      { new: true }
+    );
+    res.status(201).json({
+      success: true,
+      approval: UpdatedApproval,
     });
+  } catch (error) {
+    next(error);
   }
-  
-  const UpdatedApproval = await Approval.findByIdAndUpdate(
-    approval._id,
-    { approvalStatus: "Approved" },
-    { new: true }
-  );
-  res.status(201).json({
-    success: true,
-    approval: UpdatedApproval,
-  });
 };
 
 const disApproveEvent = async (req, res, next) => {
-  const { eventId } = req.params;
-  const { userId } = req.body;
-  const approval = await Approval.findOne({ eventId, userId });
-  const UpdatedApproval = await Approval.findByIdAndUpdate(
-    approval._id,
-    { approvalStatus: "Rejected" },
-    { new: true }
-  );
-  res.status(201).json({
-    success: true,
-    approval: UpdatedApproval,
-  });
+  try {
+    const { eventId } = req.params;
+    const { userId } = req.body;
+    const approval = await Approval.findOne({ eventId, userId });
+    const UpdatedApproval = await Approval.findByIdAndUpdate(
+      approval._id,
+      { approvalStatus: "Rejected" },
+      { new: true }
+    );
+    res.status(201).json({
+      success: true,
+      approval: UpdatedApproval,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { RequestApproveEvent, ApproveEvent, disApproveEvent };
