33cbfb8e2cc09f5ea1c47928b75c09cfbc87f5cb
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2023-02-04 23:28:15 +0530

    .

---
+++ frontend/pages/calendar/index.jsx
@@ -0,0 +1,9 @@
+import Calendar from "@components/Calendar/Calendar";
+import React from "react";
+import styles from "./calendar.module.scss";
+
+const Calendarr = () => {
+  return <Calendar committeeName="global" />;
+};
+
+export default Calendarr;
