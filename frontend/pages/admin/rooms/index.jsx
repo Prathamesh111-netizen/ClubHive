449c82cb1663b5c1efb34992e0c3eb3d160e30c3
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2023-02-04 18:32:57 +0530

    rooms :) and calendar :)

--- frontend/pages/admin/rooms/index.jsx
+++ frontend/pages/admin/rooms/index.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import React, { use, useEffect, useState } from "react";
 import Calendar from "@components/Calendar/Calendar";
 import Loader from "@components/Loader/Loader";
+import Modal from "@components/UI/Modal/Modal";
+import styles from "./rooms.module.scss";
 
 const index = () => {
   const [rooms, setRooms] = useState([]);
@@ -18,76 +20,43 @@ const index = () => {
     capacity: "",
   });
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:3000/api/rooms")
-  //     .then((response) => {
-  //       setRooms(response.data);
-  //       setIsLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // });
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/rooms/get-rooms")
+      .then((response) => {
+        setRooms(response.data.rooms.map((room) => room.roomNo));
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   // if (isLoading) {
   //   return <Loader />;
   // }
 
-  if (showModal) {
-    return (
-      <div>
-        <button onClick={setShowModal(false)}>Modal Off</button>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-          <div className="relative w-auto my-6 mx-auto max-w-3xl">
-            {/*content*/}
-            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              {/*header*/}
-              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <h3 className="text-3xl font-semibold">Modal Title</h3>
-                <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                    ×
-                  </span>
-                </button>
-              </div>
-              {/*body*/}
-              <div className="relative p-6 flex-auto">
-                <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                  I always felt like I could do anything. That’s the main thing
-                  people are controlled by! Thoughts- their perception of
-                  themselves! They're slowed down by their perception of
-                  themselves. If you're taught you can’t do anything, you won’t
-                  do anything. I was taught I could do everything.
-                </p>
-              </div>
-              {/*footer*/}
-              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Close
-                </button>
-                <button
-                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                  onClick={() => setShowModal(false)}
-                >
-                  Save Changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-      </div>
-    );
-  } else if (switchToCalendar) {
+  const addRoom = async () => {
+    let newRoom = {
+      roomNo: document.getElementById("roonNumber").value,
+      type: document.getElementById("type").value,
+      capacity: document.getElementById("capacity").value,
+    };
+    let newRooms = rooms;
+    newRooms.push(newRoom.roomNo);
+    setRooms(newRooms);
+    await axios
+      .post("http://localhost:8000/api/rooms/create-room", newRoom)
+      .then((response) => {
+        // log(response.data);
+        setShowModal(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (switchToCalendar) {
     return (
       <div>
         <div>
@@ -95,7 +64,6 @@ const index = () => {
             <h1 class="text-6xl">
               <span>Room No: </span>
               <input
-                contentEditable
                 value={roomSelected}
                 name="newRoomNo"
                 onChange={(event) => {
@@ -126,40 +94,79 @@ const index = () => {
     );
   } else {
     return (
-      <div className="ml-10">
-        <BreadCrumb />
-        <div class="min-h-screen flex items-center justify-center">
-          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
-            {rooms.map((room, i) => {
-              return (
-                <div
-                  key={i}
-                  class="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg hover:bg-green-200"
-                  onClick={() => {
-                    setRoomSelected(room);
-                    setOldRoomNo(room);
-                    let data = switchToCalendar;
-                    // setIsLoading(true);
-                    setSwitchToCalendar(!data);
-                  }}
-                >
-                  {room}
-                </div>
-              );
-            })}
-            <div
-              onClick={() => {
-                console.log("clicked");
-                setShowModal(true);
-                // setRooms([...rooms, rooms.length + 1]);
-              }}
-              class="flex justify-center items-center align-baseline w-24 h-24 bg-green-100 text-green-500 text-lg font-bold text-center rounded-full"
-            >
-              <button>+</button>
+      <>
+        <Modal
+          show={showModal}
+          hideBackdrop={() => setShowModal(false)}
+          name="add-user"
+        >
+          <div className={styles.User_form}>
+            <h1 className="text-2xl font-bold">Add Room</h1>
+            <form className="flex flex-col gap-5 mt-5">
+              <div className={styles.form_item}>
+                <label htmlFor="roonNumber" placeholder="Room Number">
+                  Room Number
+                </label>
+                <input type="text" name="roonNumber" id="roonNumber" />
+              </div>
+              <div className={styles.form_item}>
+                <label htmlFor="capacity" placeholder="Capacity">
+                  Capacity
+                </label>
+                <input type="text" name="capacity" id="capacity" />
+              </div>
+              <div className={styles.form_item}>
+                <label htmlFor="type">Type</label>
+                <select name="type" id="type">
+                  <option value="lab">Lab</option>
+                  <option value="hall">Hall</option>
+                  <option value="classroom">ClassRoom</option>
+                  <option value="auditorium">Auditorium</option>
+                  <option value="ground">Ground</option>
+                </select>
+              </div>
+
+              <button onClick={addRoom} className={styles.btn_primary}>
+                Add Room
+              </button>
+            </form>
+          </div>
+        </Modal>
+        <div className="ml-10">
+          <BreadCrumb />
+          <div class="min-h-screen flex items-center justify-center">
+            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
+              {rooms.map((room, i) => {
+                return (
+                  <div
+                    key={i}
+                    class="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg hover:bg-green-200"
+                    onClick={() => {
+                      setRoomSelected(room);
+                      setOldRoomNo(room);
+                      let data = switchToCalendar;
+                      // setIsLoading(true);
+                      setSwitchToCalendar(!data);
+                    }}
+                  >
+                    {room}
+                  </div>
+                );
+              })}
+              <div
+                onClick={() => {
+                  console.log("clicked");
+                  setShowModal(true);
+                  // setRooms([...rooms, rooms.length + 1]);
+                }}
+                class="flex justify-center items-center align-baseline w-24 h-24 bg-green-100 text-green-500 text-lg font-bold text-center rounded-full"
+              >
+                <button>+</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 };
