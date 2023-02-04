93038a24d598baef4b755717551fdfbfe4488242
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-02-05 02:15:51 +0530

    req

--- server/models/eventRegistration.model.js
+++ server/models/eventRegistration.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 var Event = mongoose.Schema({
   eventId: { type: String, required: true },
   userId: { type: String, required: true },
-  status: { type: String, required: true },
+  status: { type: String, default : "Registered" },
   createdDate: { type: Date, default: Date.now },
   updatedDate: { type: Date, default: Date.now },
 });
