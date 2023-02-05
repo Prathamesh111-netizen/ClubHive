eb9bdcced04a9cd6700fa0200c70853338d7619e
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-02-05 08:54:00 +0530

    all reg

--- server/controllers/event.reg.controllers.js
+++ server/controllers/event.reg.controllers.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import EventRegistration from "../models/eventRegistration.model.js";
+import Event from "../models/event.model.js";
+import User from "../models/user.model.js";
 
 dotenv.config();
 
 const registerForEvent = async (req, res, next) => {
   try {
-    const { userId  } = req.params;
+    const { userId } = req.params;
     const { eventId } = req.body;
     if (eventId && userId) {
       const registration = await EventRegistration.create({ eventId, userId });
@@ -34,4 +36,37 @@ const allRegisteredEvents = async (req, res, next) => {
   }
 };
 
-export { registerForEvent, allRegisteredEvents };
+const getAllregisteredUsers = async (req, res, next) => {
+  try {
+    const { committee } = req.params;
+    const events = await Event.find({ committee: committee });
+    var allregistrations = [];
+    for (var event of events) {
+      const registrations = await EventRegistration.find({
+        eventId: event._id,
+      });
+      if (registrations.length > 0) {
+        for (var reg of registrations) {
+          if(reg)
+          allregistrations.push(reg.userId);
+        }
+      }
+    }
+    console.log(allregistrations);
+    
+    var registeredusers = [];
+    for (var reg of allregistrations) {
+      const user = await User.findById(reg);
+      if (user)
+      registeredusers.push(user);
+    }
+    res.status(200).json({
+      success: true,
+      registrations: registeredusers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { registerForEvent, allRegisteredEvents, getAllregisteredUsers };
