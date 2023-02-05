503f62075bc95badebcdd59f47bcd6629b8787fc
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-02-05 05:42:25 +0530

    ev

--- server/controllers/eventControllers.js
+++ server/controllers/eventControllers.js
@@ -80,7 +80,7 @@ const getEventByCategory = async (req, res, next) => {
 const getEventByCommittee = async (req, res, next) => {
   try {
     const { committee } = req.query;
-    const event = await Event.find({ committee: committee });
+    const event = await Event.find({ committee: committee, approvalStatus : "Pending" });
     if (!event) {
       res.status(400).json({
         success: false,
@@ -147,7 +147,7 @@ const createEvent = async (req, res, next) => {
 
     // send emails all faculty mentors , dean academics, president
     sendEmail("2002pratham1109@gmail.com");
-    
+
   } catch (error) {
     next(error);
   }
